3105584166ad81f211aee6db428620abb4fcc6be
php1301 <luudanthanchet@gmail.com>
2020-10-19 09:31:36 +0700

    SideNavigator Item added

--- lib/widgets/side_navigator_item.dart
+++ lib/widgets/side_navigator_item.dart
@@ -1,32 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class SideNavigatorItem extends StatefulWidget {
+class SideNavigatorItem extends StatelessWidget {
   final Map<String, dynamic> item;
-  SideNavigatorItem(this.item);
+  final Function switchItem;
+  SideNavigatorItem(this.item, this.switchItem);
 
-  @override
-  _SideNavigatorItemState createState() => _SideNavigatorItemState();
-}
-
-class _SideNavigatorItemState extends State<SideNavigatorItem> {
   @override
   Widget build(BuildContext context) {
-    return widget.item['isChecked']
-        ? Container(
-            height: 8.0,
-            width: 8.0,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: Color(0xFF23163D),
-            ),
-          )
-        : Container(
-            height: 8.0,
-            width: 8.0,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.transparent,
-            ),
-          );
+    return Column(
+      children: [
+        item['isChoose']
+            ? Container(
+                height: 8.0,
+                width: 8.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Color(0xFF23163D),
+                ),
+              )
+            : Container(
+                height: 8.0,
+                width: 8.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.transparent,
+                ),
+              ),
+        SizedBox(
+          height: 15.0,
+        ),
+        GestureDetector(
+          onTap: () => switchItem(item['id']),
+          child: Text(
+            item['text'],
+            style: item['isChoose']
+                ? GoogleFonts.bigShouldersText(
+                    color: Color(0xFF23163D),
+                    fontSize: 18.0,
+                  )
+                : GoogleFonts.bigShouldersText(
+                    color: Color(0xFFA59FB0),
+                    fontSize: 18.0,
+                  ),
+          ),
+        )
+      ],
+    );
   }
 }
