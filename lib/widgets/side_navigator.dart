ec99742ab1a2e15d9874ee79055de16e0987bfc5
php1301 <luudanthanchet@gmail.com>
2020-10-19 09:31:27 +0700

    SideNavigator added

--- lib/widgets/side_navigator.dart
+++ lib/widgets/side_navigator.dart
@@ -1,52 +1,27 @@
 import 'package:UITrain/widgets/side_navigator_item.dart';
 import 'package:flutter/material.dart';
 
-class SideNavigator extends StatefulWidget {
-  final mediaHeight;
-  final mediaWidth;
+class SideNavigator extends StatelessWidget {
   final List<Map<String, dynamic>> navigateItems;
+  final Function switchItem;
+  SideNavigator(this.navigateItems, this.switchItem);
 
-  SideNavigator(this.mediaHeight, this.mediaWidth, this.navigateItems);
-
-  @override
-  _SideNavigatorState createState() => _SideNavigatorState();
-}
-
-class _SideNavigatorState extends State<SideNavigator> {
   @override
   Widget build(BuildContext context) {
-    print(widget.navigateItems[0]['text']);
     return Positioned(
       top: 75.0,
       child: RotatedBox(
         quarterTurns: 3,
         child: Container(
-            width: widget.mediaWidth - 100.0,
-            height: widget.mediaHeight / 5,
+            width: MediaQuery.of(context).size.height - 100.0,
+            height: MediaQuery.of(context).size.width / 5,
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                widget.navigateItems.isEmpty == false
-                    ? ListView.builder(
-                        itemBuilder: (ctx, i) {
-                          return Column(
-                            children: [
-                              SideNavigatorItem(widget.navigateItems[i]),
-                              SizedBox(
-                                height: 5.0,
-                              ),
-                              GestureDetector(
-                                onTap: () {},
-                                child: Text(widget.navigateItems[i]['text']),
-                              )
-                            ],
-                          );
-                        },
-                        itemCount: widget.navigateItems.length,
-                      )
-                    : Text('none'),
-              ],
-            )),
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ...navigateItems
+                      .map((e) => SideNavigatorItem(e, switchItem))
+                      .toList()
+                ])),
       ),
     );
   }
